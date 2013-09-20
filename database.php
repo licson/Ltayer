5269ad211af97ee3b2656660d0dd9fe64e2de5c5
s3131212 <s3131212@gmail.com>
2013-09-20 19:16:35 +0800

    Update database.php

--- database.php
+++ database.php
@@ -3,13 +3,13 @@ include('class/sql.class.php');
 //include('config.php');
 
 //Enter sql host
-$config['sql']['host'] = 'nas.s3131212.com';
+$config['sql']['host'] = 'Mysql伺服器';
 //Enter sql database name
-$config['sql']['dbname'] = 'allenos';
+$config['sql']['dbname'] = 'Mysql資料庫名稱';
 //Enter database username
-$config['sql']['username'] = 'webos';
+$config['sql']['username'] = 'Mysql帳號';
 //Enter database password
-$config['sql']['password'] = '123';
+$config['sql']['password'] = 'Mysql密碼';
 
 //Initialization sql info
 $db = new MySQL($config['sql']['dbname'], $config['sql']['username'], $config['sql']['password'], $config['sql']['host']);
