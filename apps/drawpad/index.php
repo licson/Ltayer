ca30b34592f7f4bba9af155faa022eacf5a6a3e5
s3131212 <s3131212@gmail.com>
2014-01-01 16:53:34 +0800

    Update index.php

--- apps/drawpad/index.php
+++ apps/drawpad/index.php
@@ -1,25 +1,27 @@
 <?php require_once('../../core/require.php'); ?>
 <!DOCTYPE html>
-
 <html>
 	<head>
 		<title>小畫家</title>
 		<link rel="stylesheet" href="../../css/jquery-ui-1.10.3.custom.min.css" />
 		<style>
-			body, input {
+			body {
 				font-size: 9pt;
 				margin: 0;
 				padding: 0;
 			}
-			#dCanvas, #dLine {
+			
+			#line {
 				clear: both;
 			}
-			#dCanvas {
+			
+			#paintarea {
 				position: fixed;
 				top: 0;
 				left: 0;
 				z-index: -99;
 			}
+			
 			#controls {
 				position: fixed;
 				width: auto;
@@ -28,24 +30,26 @@
 				top: 1em;
 				left: 1em;
 			}
+			
 			.option {
-				float: left; width: 20px; height: 20px; border: 2px solid #cccccc;
-				margin-right: 4px; margin-bottom: 4px;
+				float: left;
+				width: 20px;
+				height: 20px;
+				margin-right: 4px;
+				margin-bottom: 4px;
+				box-shadow: 0 0 4px #aaa;
 			}
+			
+			.paint_controls {
+			    background-color: #fff;
+			    box-shadow: 0 0 15px #ccc;
+			}
+			
 			.active {
-				border: 2px solid black;
+				box-shadow: 0 0 4px #000;
 			}
-			.lw {
-				text-align: center;
-				vertical-align: middle;
-			}
-			img {
-				box-shadow: 0 0 10px #ccc;
-				display: block;
-				margin: 1em auto;
-				width: 80%;
-			}
-			#cSketchPad {
+			
+			#paint {
 				cursor: arrow;
 			}
 		</style>
@@ -54,117 +58,144 @@
 		<script src="../../js/jquery.ui.touch.min.js"></script>
 		<script>
 			$(function () {
-                var p_width, p_color;
-				//產生不同顏色的div方格當作調色盤選項
-				var colors = "red;orange;yellow;green;blue;indigo;purple;black;white".split(';');
-				var sb = [];
-				$.each(colors, function (i, v) {
-					sb.push("<div class='option' style='background-color:" + v + "'></div>");
+				var p_width, p_color;
+				var colors = ['#F00', '#0F0', '#00F', '#FF0', '#0FF', '#F0F', '#F90', '#FFF'];
+				$.each(colors, function (i) {
+					$("<div class='option' style='background-color:" + colors[i] + "'></div>").appendTo('#pallete');
 				});
-				$("#dPallete").html(sb.join("\n"));
-				
-				//產生不同尺寸的方格當作線條粗細選項
-				sb = [];
-				for (var i = 1; i <= 9; i++){
-                    sb.push(
-                        "<div class='option lw'>" +
-                        "<div style='margin-top:#px;margin-left:#px;width:%px;height:%px'></div></div>"
-                        .replace(/%/g, i)
-                        .replace(/#/g, 10 - i / 2));
+
+				for(var i = 1; i <= 9; i++) {
+					$('<div class="option"><div></div></div>')
+					.find('div')
+                    .css({
+                        'margin-top': 10 - i / 2,
+                        'margin-left': 10 - i / 2,
+                        width: i,
+                        height: i
+                    })
+					.end()
+					.appendTo('#line');
 				}
-				$("#dLine").html(sb.join('\n'));
-				var $clrs = $("#dPallete .option");
-				var $lws = $("#dLine .option");
-				
-				//點選調色盤時切換焦點並取得顏色存入p_color，
-				//同時變更線條粗細選項的方格的顏色
-				$clrs.click(function () {
-					$clrs.removeClass("active");
-					$(this).addClass("active");
-					p_color = this.style.backgroundColor;
-					$lws.children("div").css("background-color", p_color);
-				}).first().click();
-				
-				//點選線條粗細選項時切換焦點並取得寬度存入p_width
-				$lws.click(function () {
-					$lws.removeClass("active");
-					$(this).addClass("active");
-					p_width = $(this).children("div").css("width").replace("px", "");
-	
-				}).eq(3).click();
-				
-				//取得canvas context
-				var $canvas = $("#cSketchPad");
+
+				var cBlock = $("#pallete .option");
+				var lBlock = $("#line .option");
+
+				cBlock
+					.click(function () {
+						cBlock.removeClass("active");
+						$(this).addClass("active");
+						
+						p_color = $(this).css('background-color');
+						
+						lBlock
+                            .children("div")
+                            .css("background-color", p_color);
+					})
+					.first()
+					.click();
+
+				lBlock
+					.click(function () {
+						lBlock.removeClass("active");
+						$(this).addClass("active");
+						p_width = $(this)
+                            .children("div")
+                            .css("width")
+                            .replace("px", "");
+					})
+					.eq(3)
+					.click();
+
+				var $canvas = $("#paint");
 				var ctx = $canvas[0].getContext("2d");
-				
+
 				$canvas[0].width = window.innerWidth;
 				$canvas[0].height = window.innerHeight;
-				
-				$(window).on('resize',function(){
-                    var state = ctx.getImageData(0, 0, $canvas[0].width, $canvas[0].height);
+
+				$(window).on('resize', function () {
+					var state = ctx.getImageData(0, 0, $canvas[0].width, $canvas[0].height);
+					
 					$canvas[0].width = window.innerWidth;
 					$canvas[0].height = window.innerHeight;
+					
 					ctx.putImageData(state, 0, 0);
 				});
-				
-				ctx.lineCap = "round";
-				ctx.fillStyle = "white"; //整個canvas塗上白色背景避免PNG的透明底色效果
+
+				ctx.lineCap = "butt";
+				ctx.fillStyle = "white";
 				ctx.fillRect(0, 0, $canvas.width(), $canvas.height());
-				var drawMode = false;
-				
-				//canvas點選、移動、放開按鍵事件時進行繪圖動作
-				$canvas.on('mousedown touchstart', function(e){
-					ctx.beginPath();
-					ctx.strokeStyle = p_color;
-					ctx.lineWidth = p_width;
-					ctx.moveTo((e.pageX || e.originalEvent.targetTouches[0].pageX) - $canvas.position().left, (e.pageY || e.originalEvent.targetTouches[0].pageY) - $canvas.position().top);
-					drawMode = true;
-				})
-				.on('mousemove touchmove', function(e){
-					if (drawMode) {
-						ctx.lineTo((e.pageX || e.originalEvent.targetTouches[0].pageX) - $canvas.position().left, (e.pageY || e.originalEvent.targetTouches[0].pageY) - $canvas.position().top);
-						ctx.stroke();
-					}
-				})
-				.on('mouseup touchend', function(e){
-					drawMode = false;
+				var mousedown = false;
+
+				$canvas
+					.on('mousedown touchstart', function (e) {
+						ctx.beginPath();
+						ctx.strokeStyle = p_color;
+						ctx.lineWidth = p_width;
+
+						var posX = (e.pageX || e.originalEvent.targetTouches[0].pageX);
+						var posY = (e.pageY || e.originalEvent.targetTouches[0].pageY);
+
+						ctx.moveTo(posX, posY);
+						mousedown = true;
+					})
+					.on('mousemove touchmove', function (e) {
+						if(mousedown) {
+							var posX = (e.pageX || e.originalEvent.targetTouches[0].pageX);
+							var posY = (e.pageY || e.originalEvent.targetTouches[0].pageY);
+
+							ctx.lineTo(posX, posY);
+							ctx.stroke();
+						}
+					})
+					.on('mouseup touchend', function (e) {
+						mousedown = false;
+					});
+
+				$("#saveImg").button().click(function () {
+					var data = escape($canvas[0].toDataURL('image/png').split(',')[1]);
+					var xhr = new XMLHttpRequest();
+
+					xhr.onreadystatechange = function () {
+						if(xhr.status === 200 && xhr.readyState == 4) {
+							$("#output").dialog("open");
+						}
+					};
+
+					xhr.open("POST", "./save.php", true);
+					xhr.send(data);
 				});
-				
-				$("#bGenImage").button().click(function (){
-                    var data = escape($canvas[0].toDataURL('image/png').split(',')[1]);
-                    var xhr = new XMLHttpRequest();
-                    
-                    xhr.onreadystatechange = function(){
-                        if(xhr.status === 200 && xhr.readyState == 4){
-                            $("#dOutput").dialog("open");
-                        }
-                    };
-                    
-                    xhr.open("POST", "./save.php", true);
-                    xhr.send(data);
-				});
-				
-				$("#dOutput").dialog({
+
+				$("#output").dialog({
 					autoOpen: false,
 					modal: true
 				});
-				
+
 				$("#controls").draggable({
 					containment: "parent",
-					scroll: false
+					scroll: false,
+					start: function(){
+                        $(this).animate({
+                            opacity: 1
+                        }, 500);
+					},
+					stop: function(){
+                        $(this).animate({
+                            opacity: 0.5
+                        }, 500);
+					}
 				});
 			});
 		</script>
 	</head>
 	<body>
-		<div class="ui-state-default ui-corner-all ui-helper-clearfix" id="controls">
-			<div id="dPallete"></div>
-			<div id="dLine"></div>
-			<input type="button" id="bGenImage" value="儲存圖片" />
+		<div class="paint_controls ui-corner-all ui-helper-clearfix" id="controls">
+			<div id="pallete"></div>
+			<div id="line"></div>
+			<input type="button" id="saveImg" value="儲存圖片" />
 		</div>
-		<div id="dCanvas">
-			<canvas id="cSketchPad" width="100%" height="600" />
+		<div id="paintarea">
+			<canvas id="paint" width="100%" height="600" />
 		</div>
-		<div id="dOutput" title="完成">儲存完成!</div>
+		<div id="output" title="完成">儲存完成!</div>
 	</body>
 </html>
